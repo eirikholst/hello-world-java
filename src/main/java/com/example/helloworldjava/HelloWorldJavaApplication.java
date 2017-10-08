22ffba61007d3746ceaacf1bfdb09d5e65be4cf5
Eirik Holst <eirik.holst@bouvet.no>
2017-10-08 16:51:11 +0200

    Initial commit

---
+++ src/main/java/com/example/helloworldjava/HelloWorldJavaApplication.java
@@ -0,0 +1,12 @@
+package com.example.helloworldjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldJavaApplication.class, args);
+	}
+}
